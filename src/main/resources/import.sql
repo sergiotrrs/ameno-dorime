d344b9c9722ed61aae3344ecc0632408018cbcd9
Sergio Torres <sergio.torres@idr.edu.mx>
2022-08-15 16:23:36 -0500

    Nuevo script sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,36 +1,78 @@
-/*INSERT INTO customer_order_products (order_order_id, products_product_id) VALUES (1,1);
-INSERT INTO customer_order_products (order_order_id, products_product_id) VALUES (1,2);
-INSERT INTO customer_order_products (order_order_id, products_product_id) VALUES (1,3);
-INSERT INTO customer_order_products (order_order_id, products_product_id) VALUES (2,1);
-INSERT INTO customer_order_products (order_order_id, products_product_id) VALUES (3,2);
-INSERT INTO customer_order_products (order_order_id, products_product_id) VALUES (4,3);
-INSERT INTO customer_order_products (order_order_id, products_product_id) VALUES (5,2);
-INSERT INTO customer_order_products (order_order_id, products_product_id) VALUES (5,3);
-*/
+
+
+
 /* Se insertan los siguientes registros en la tabla */
 
-INSERT INTO customer_data (first_name, last_name) VALUES('Benito', 'Bodoque');
-INSERT INTO customer_data (first_name, last_name) VALUES('Reese', 'Wilkerson');
-INSERT INTO customer_data (first_name, last_name) VALUES('Nelson', 'Montz');
-INSERT INTO customer_data (first_name, last_name) VALUES('Leonel', 'Messi');
-INSERT INTO customer_data (first_name, last_name) VALUES('Rick', 'Sánchez');
+INSERT INTO customer_role (role_type, description) VALUES ('client','Cliente del sitio');
+INSERT INTO customer_role (role_type, description) VALUES ('admin','Adminiestrador del sitio');
+INSERT INTO customer_role (role_type, description) VALUES ('owner','Propietario del sitio');
 
-INSERT INTO customer_data (first_name, last_name) VALUES('Cristiano', 'Ronaldo');
-INSERT INTO customer_data (first_name, last_name) VALUES('Lois', 'Wilkerson');
-INSERT INTO customer_data (first_name, last_name) VALUES('Perlita', 'Cangrejo');
-INSERT INTO customer_data (first_name, last_name) VALUES('Patricio', 'Estrella');
-INSERT INTO customer_data (first_name, last_name) VALUES('Espergencia', 'Valdés');
+INSERT INTO customer_data (first_name, last_name, role_role_id) VALUES('Benito', 'Bodoque', 3);
+INSERT INTO customer_data (first_name, last_name, role_role_id) VALUES('Reese', 'Wilkerson',1);
+INSERT INTO customer_data (first_name, last_name, role_role_id) VALUES('Nelson', 'Montz',1);
+INSERT INTO customer_data (first_name, last_name, role_role_id) VALUES('Leonel', 'Messi',2);
+INSERT INTO customer_data (first_name, last_name, role_role_id) VALUES('Rick', 'Sánchez',1);
 
-INSERT INTO customer_data (first_name, last_name) VALUES('Edna', 'Krabappel');
-INSERT INTO customer_data (first_name, last_name) VALUES('Lois', 'Wilkerson');
-INSERT INTO customer_data (first_name, last_name) VALUES('Eric', 'Cartman');
-INSERT INTO customer_data (first_name, last_name) VALUES('Kenny', 'McCormick');
-INSERT INTO customer_data (first_name, last_name) VALUES('Thomas', 'Anderson');
+INSERT INTO customer_data (first_name, last_name, role_role_id) VALUES('Cristiano', 'Ronaldo',2);
+INSERT INTO customer_data (first_name, last_name, role_role_id) VALUES('Lois', 'Wilkerson',1);
+INSERT INTO customer_data (first_name, last_name, role_role_id) VALUES('Perlita', 'Cangrejo',null);
+INSERT INTO customer_data (first_name, last_name, role_role_id) VALUES('Patricio', 'Estrella',1);
+INSERT INTO customer_data (first_name, last_name, role_role_id) VALUES('Espergencia', 'Valdés',null);
 
-INSERT INTO customer_data (first_name, last_name) VALUES('Marty', 'McFly');
-INSERT INTO customer_data (first_name, last_name) VALUES('Saul', 'Goodman');
-INSERT INTO customer_data (first_name, last_name) VALUES('Federico', 'Bardón');
-INSERT INTO customer_data (first_name, last_name) VALUES('Walter', 'White');
-INSERT INTO customer_data (first_name, last_name) VALUES('Emmett', 'Brown');
+INSERT INTO customer_data (first_name, last_name, role_role_id) VALUES('Edna', 'Krabappel',1);
+INSERT INTO customer_data (first_name, last_name, role_role_id) VALUES('Lois', 'Wilkerson',1);
+INSERT INTO customer_data (first_name, last_name, role_role_id) VALUES('Eric', 'Cartman',1);
+INSERT INTO customer_data (first_name, last_name, role_role_id) VALUES('Kenny', 'McCormick',1);
+INSERT INTO customer_data (first_name, last_name, role_role_id) VALUES('Thomas', 'Anderson',3);
 
+INSERT INTO customer_data (first_name, last_name, role_role_id) VALUES('Marty', 'McFly',1);
+INSERT INTO customer_data (first_name, last_name, role_role_id) VALUES('Saul', 'Goodman',1);
+INSERT INTO customer_data (first_name, last_name, role_role_id) VALUES('Federico', 'Bardón',2);
+INSERT INTO customer_data (first_name, last_name, role_role_id) VALUES('Walter', 'White',null);
+INSERT INTO customer_data (first_name, last_name, role_role_id) VALUES('Emmett', 'Brown', null);
 
+INSERT INTO product_list (description, price) VALUES ('Play Station',15278.53);
+INSERT INTO product_list (description, price) VALUES ('Xbox',13245.52);
+INSERT INTO product_list (description, price) VALUES ('Casio FX-991EX',427.41);
+INSERT INTO product_list (description, price) VALUES ('2021 Apple iPad de 10.2',7671.65);
+INSERT INTO product_list (description, price) VALUES ('PlaLogitech G305 LIGHTSPEED ',1199.78);
+INSERT INTO product_list (description, price) VALUES ('SAMSUNG LS24R350FZLXZX',2899);
+INSERT INTO product_list (description, price) VALUES ('Power Bank 10000 mAh',395);
+INSERT INTO product_list (description, price) VALUES ('Xiaomi REDMI Note 11',4176.1);
+INSERT INTO product_list (description, price) VALUES ('Apple Nuevo AirTag',2299);
+INSERT INTO product_list (description, price) VALUES ('Bose SoundLink Revolve+',5389.3);
+INSERT INTO product_list (description, price) VALUES ('SAMSUNG Galaxy A52 128gb ',6472);
+INSERT INTO product_list (description, price) VALUES ('Apple Pencil ',3199);
+
+INSERT INTO customer_order (status, description, customer_id_customer) VALUES ('p','pending', 1);
+INSERT INTO customer_order (status, description, customer_id_customer) VALUES ('c','confirmed',2);
+INSERT INTO customer_order (status, description, customer_id_customer) VALUES ('x','cancelled',2);
+INSERT INTO customer_order (status, description, customer_id_customer) VALUES ('r','refunded',3);
+INSERT INTO customer_order (status, description, customer_id_customer) VALUES ('s','shipped',3);
+INSERT INTO customer_order (status, description, customer_id_customer) VALUES ('rd','ready for delivery',3);
+INSERT INTO customer_order (status, description, customer_id_customer) VALUES ('p','pending', 4);
+INSERT INTO customer_order (status, description, customer_id_customer) VALUES ('c','confirmed',5);
+INSERT INTO customer_order (status, description, customer_id_customer) VALUES ('x','cancelled',5);
+INSERT INTO customer_order (status, description, customer_id_customer) VALUES ('r','refunded',5);
+INSERT INTO customer_order (status, description, customer_id_customer) VALUES ('s','shipped',6);
+INSERT INTO customer_order (status, description, customer_id_customer) VALUES ('rd','ready for delivery',6)
+INSERT INTO customer_order (status, description, customer_id_customer) VALUES ('p','pending', 7);
+INSERT INTO customer_order (status, description, customer_id_customer) VALUES ('c','confirmed',7);
+INSERT INTO customer_order (status, description, customer_id_customer) VALUES ('x','cancelled',7);
+INSERT INTO customer_order (status, description, customer_id_customer) VALUES ('r','refunded',8);
+INSERT INTO customer_order (status, description, customer_id_customer) VALUES ('s','shipped',8);
+INSERT INTO customer_order (status, description, customer_id_customer) VALUES ('rd','ready for delivery',8)
+
+INSERT INTO customer_order_products (order_order_id, products_id_product) VALUES (1,1);
+INSERT INTO customer_order_products (order_order_id, products_id_product) VALUES (1,2);
+INSERT INTO customer_order_products (order_order_id, products_id_product) VALUES (1,3);
+INSERT INTO customer_order_products (order_order_id, products_id_product) VALUES (2,1);
+INSERT INTO customer_order_products (order_order_id, products_id_product) VALUES (3,2);
+INSERT INTO customer_order_products (order_order_id, products_id_product) VALUES (4,3);
+INSERT INTO customer_order_products (order_order_id, products_id_product) VALUES (5,2);
+INSERT INTO customer_order_products (order_order_id, products_id_product) VALUES (5,3);
+INSERT INTO customer_order_products (order_order_id, products_id_product) VALUES (6,2);
+INSERT INTO customer_order_products (order_order_id, products_id_product) VALUES (6,3);
+INSERT INTO customer_order_products (order_order_id, products_id_product) VALUES (6,1);
+INSERT INTO customer_order_products (order_order_id, products_id_product) VALUES (7,2);
+INSERT INTO customer_order_products (order_order_id, products_id_product) VALUES (8,2);
